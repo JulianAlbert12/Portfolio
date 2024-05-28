204c8b0da230cfbb9eeda49971e9f7e4a7f93b1d
jxa <julianalbert0012@gmail.com>
2024-05-27 17:30:45 -0700

    Added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,11 +7,27 @@ const Projects = () => {
       <h2>Projects</h2>
       <ul>
         <li>
-          <h3>Project Title</h3>
-          <p>Project Description</p>
-          <a href="github.com/JulianAlbert12">GitHub Link</a>
+          <div className="project-content">
+            <div className="project-text">
+              <h3>Net Vision</h3>
+              <p>This project’s main goal is to create a web-based interface for conducting port scans on target IP addresses, using both a frontend developed using React.js and a backend API built with Flask.</p>
+              <a href="https://github.com/JulianAlbert12/NetVision">GitHub Link</a>
+            </div>
+            <div className="project-image">
+            <img src={`${process.env.PUBLIC_URL}/TCPoutput.png`} alt="Output" />
+            </div>
+          </div>
         </li>
-        {/* Add more projects as needed */}
+        <div className="project-content">
+            <div className="project-text">
+              <h3>Net Vision</h3>
+              <p>This project’s main goal is to create a web-based interface for conducting port scans on target IP addresses, using both a frontend developed using React.js and a backend API built with Flask.</p>
+              <a href="https://github.com/JulianAlbert12/NetVision">GitHub Link</a>
+            </div>
+            <div className="project-image">
+            <img src={`${process.env.PUBLIC_URL}/TCPoutput.png`} alt="Output" />
+            </div>
+          </div>
       </ul>
     </section>
   );

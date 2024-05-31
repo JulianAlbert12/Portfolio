6516bb714017ead162aa164ec3a790e0eb20b52a
jxa <julianalbert0012@gmail.com>
2024-05-30 20:34:37 -0700

    updated styles

--- src/components/About.js
+++ src/components/About.js
@@ -3,15 +3,30 @@ import './About.css';
 
 const About = () => {
   return (
-    <section className="about">
-      <h2>About Me</h2>
-      <p>
-        Currently studying computer science with a focus on computer security at the University of Oregon, gaining expertise in securing computer systems and networks.
-      </p>
-      <p>
-        With a customer service and sales background, I bring strong problem-solving skills and attention to detail to my work.
-      </p>
-    </section>
+    <div className='About'>
+      <section className="text">
+        <h2>About Me</h2>
+        <p>
+            Currently studying computer science with a focus on computer security at the University of Oregon, gaining expertise in securing computer systems and networks. As a soon-to-be graduate, I am eager to apply my diverse skill set and passion for technology to entry-level IT roles.
+        </p>
+        <br></br>
+        <p>  
+            Throughout my academic journey, I have cultivated a strong foundation in computer science, with a focus on computer security, network architecture, and cryptography. My coursework has equipped me with practical knowledge in troubleshooting hardware and software issues, configuring network devices, and implementing security measures to safeguard sensitive data.
+        </p>
+        <br></br>
+        <p>
+            In addition to my academic pursuits, I have gained hands-on experience in customer service roles, where I developed excellent communication and problem-solving skills. These skills, coupled with my technical expertise, make me well-prepared to excel in IT helpdesk/support roles, where I can leverage my ability to assist users with technical issues and provide timely resolutions.
+        </p>
+        <br></br>
+        <p>
+            Furthermore, my passion for cybersecurity drives my interest in pursuing entry-level security analyst positions. I am eager to apply my knowledge of security principles and protocols to identify and mitigate potential security threats, ensuring the integrity and confidentiality of organizational data.
+        </p>
+        <br></br>
+        <p>
+            I am enthusiastic about the opportunity to contribute to a dynamic IT team and continue learning and growing in the field of information technology.
+        </p>
+      </section>
+    </div>
   );
 };
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,25 +9,17 @@ const Projects = () => {
         <li>
           <div className="project-content">
             <div className="project-text">
-              <h3>Net Vision</h3>
-              <p>This project’s main goal is to create a web-based interface for conducting port scans on target IP addresses, using both a frontend developed using React.js and a backend API built with Flask.</p>
-              <a href="https://github.com/JulianAlbert12/NetVision">GitHub Link</a>
+              <a href="https://github.com/JulianAlbert12/NetVision" className="project-link">
+                <h3>Net Vision</h3>
+                <p>This project’s main goal is to create a web-based interface for conducting port scans on target IP addresses, using both a frontend developed using React.js and a backend API built with Flask.</p>
+                <p classname="read">See more...</p>
+              </a>
             </div>
             <div className="project-image">
-            <img src={`${process.env.PUBLIC_URL}/TCPoutput.png`} alt="Output" />
+              <img src={`${process.env.PUBLIC_URL}/TCPoutput.png`} alt="Output" />
             </div>
           </div>
         </li>
-        <div className="project-content">
-            <div className="project-text">
-              <h3>Net Vision</h3>
-              <p>This project’s main goal is to create a web-based interface for conducting port scans on target IP addresses, using both a frontend developed using React.js and a backend API built with Flask.</p>
-              <a href="https://github.com/JulianAlbert12/NetVision">GitHub Link</a>
-            </div>
-            <div className="project-image">
-            <img src={`${process.env.PUBLIC_URL}/TCPoutput.png`} alt="Output" />
-            </div>
-          </div>
       </ul>
     </section>
   );

59312d502ef207bbf95ca3ab6a3c5cff7e81292e
jxa <julianalbert0012@gmail.com>
2024-05-31 12:44:26 -0700

    updated sizing

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,27 +8,47 @@ const Experience = () => {
       <ul>
         <li>
           <h3>Operations Manager</h3>
-          <p>University of Oregon Student Recreation Center</p>
-          <p>March 2023 - June 2024</p>
-          <p>Assist in the supervision of all program areas in the Student Recreation Center. Serve as the leader and point person in the SRC and satellite facilities. Overseeing all operations staff.</p>
+            <p>University of Oregon Student Recreation Center</p>
+          <div className="location-date">
+            <img src={`${process.env.PUBLIC_URL}/calendar.png`} alt="Date Icon" />
+            <p>March 2023 - June 2024</p>
+          </div>
+          <p className="description">
+            Assist in the supervision of all program areas in the Student Recreation Center. Serve as the leader and point person in the SRC and satellite facilities. Overseeing all operations staff.
+          </p>
         </li>
         <li>
           <h3>Freelance Web Developer</h3>
-          <p>Hagel Supply</p>
-          <p>June 2023 - October 2024</p>
-          <p>Developed, optimized, and maintained the company website utilizing HTML, CSS, JavaScript, Google Firestore, and several APIs to meet clients' needs and goals. Tested websites and performed user testing and troubleshooting before live deployment.</p>
+            <p>Hagel Supply</p>
+          <div className="location-date">
+            <img src={`${process.env.PUBLIC_URL}/calendar.png`} alt="Date Icon" />
+            <p>June 2023 - October 2024</p>
+          </div>
+          <p className="description">
+            Developed, optimized, and maintained the company website utilizing HTML, CSS, JavaScript, Google Firestore, and several APIs to meet clients' needs and goals. Tested websites and performed user testing and troubleshooting before live deployment.
+          </p>
         </li>
         <li>
           <h3>Operation Specialist</h3>
-          <p>University of Oregon Student Recreation Center</p>
-          <p>September 2021 - March 2023</p>
-          <p>Assist patrons with memberships, facility inquiries, and classes. Provide customer support to all patrons and staff. Lead and help operations attendants with tasks and problems.</p>
+            <p>University of Oregon Student Recreation Center</p>
+          <div className="location-date">
+            <img src={`${process.env.PUBLIC_URL}/calendar.png`} alt="Date Icon" />
+            <p>September 2021 - March 2023</p>
+          </div>
+          <p className="description">
+            Assist patrons with memberships, facility inquiries, and classes. Provide customer support to all patrons and staff. Lead and help operations attendants with tasks and problems.
+          </p>
         </li>
         <li>
           <h3>Sales Associate</h3>
-          <p>Sports Basement</p>
-          <p>April 2018 - December 2022</p>
-          <p>Delivered exceptional customer service. Assisted in POS system, fulfillment, receiving, and merchandising operations. Developed and instructed new staff members.</p>
+            <p>Sports Basement</p>
+          <div className="location-date">
+            <img src={`${process.env.PUBLIC_URL}/calendar.png`} alt="Date Icon" />
+            <p>April 2018 - December 2022</p>
+          </div>
+          <p className="description">
+            Delivered exceptional customer service. Assisted in POS system, fulfillment, receiving, and merchandising operations. Developed and instructed new staff members.
+          </p>
         </li>
       </ul>
     </section>
